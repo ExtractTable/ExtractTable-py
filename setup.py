56edd16f64fe4428c60251206d450129b96d5d23
akshowhini <33936764+akshowhini@users.noreply.github.com>
2021-07-09 00:15:39 -0400

    Modified setup (#39)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-from setuptools import find_packages
+import setuptools
 
 about = {}
 with open(os.path.join('ExtractTable', '__version__.py'), 'r') as f:
@@ -10,12 +10,8 @@ with open(os.path.join('ExtractTable', '__version__.py'), 'r') as f:
 with open('README.md', 'r') as f:
     readme = f.read()
 
-with open("requirements.txt") as fh:
-    requires = [x.strip() for x in fh.readlines()]
 
-
-def setup_package():
-    metadata = dict(
+setuptools.setup(
         name=about['__title__'],
         version=about['__version__'],
         description=about['__description__'],
@@ -25,8 +21,8 @@ def setup_package():
         author=about['__author__'],
         author_email=about['__author_email__'],
         license=about['__license__'],
-        packages=find_packages(),
-        install_requires=requires,
+        packages=setuptools.find_packages(),
+        install_requires=['requests>=2.21', 'pandas>=0.24', 'PyPDF2>=1.26'],
         classifiers=[
             # Trove classifiers
             # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -36,15 +32,6 @@ def setup_package():
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10'
-        ])
-
-    try:
-        from setuptools import setup
-    except ImportError:
-        from distutils.core import setup
-
-    setup(**metadata)
-
-
-if __name__ == '__main__':
-    setup_package()
+        ],
+        python_requires=">=3.6",
+    )
